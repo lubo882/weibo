f08e7e898cf056a2c0f2ca6540103d614bbc9191
vagrant <vagrant@homestead>
2020-01-02 13:19:16 +0000

    管理员删除权限

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'lubo882';
         $user->email = 'lubo882@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
