fc72ec8f026a12d1882e98d2cf4a499aeaa8408c
vagrant <vagrant@homestead>
2020-01-02 12:25:46 +0000

    更新个人信息

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a href="{{ route('home') }}" class="navbar-brand">Weibo App</a>
+    <div class="container ">
+        <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
         <ul class="navbar-nav justify-content-end">
             @if (Auth::check())
                 <li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
@@ -10,13 +10,13 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
-                        <a class="dropdown-item" href="#">编辑资料</a>
+                        <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">编辑资料</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" id="logout" href="#">
                             <form action="{{ route('logout') }}" method="POST">
                                 {{ csrf_field() }}
                                 {{ method_field('DELETE') }}
-                                <button class="btn btn-block btn-danger btn-sm" type="submit" name="button">退出</button>
+                                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
                             </form>
                         </a>
                     </div>
